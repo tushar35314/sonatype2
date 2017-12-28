d44fd7a3f76ec366ebdf399c25299df934cf833e
Tushar Ranjan Mishra <tushar35314@gmail.com>
2017-12-29 04:59:41 +0530

    Initial Commit

---
+++ src/main/java/org/sonatype2/maven/App.java
@@ -0,0 +1,13 @@
+package org.sonatype2.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
